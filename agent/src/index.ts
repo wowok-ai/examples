f5d9cbe62c72b4fa6517e5f52f44b9d6a4a503e1
wowok <enoloo@126.com>
2025-03-10 12:07:37 +0800

    fix

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -4,8 +4,18 @@ import { test_agent_objects } from './query_test';
 import { test_call } from './call_test';
 import { airdrop } from './airdrop';
 import { e_commerce } from './e-commerce';
+import { Account } from 'wowok_agent';
 
 const main = async () => {
+
+    if (!Account.Instance().get_address()) {
+        Account.Instance().gen('my default account', true);
+        await Account.Instance().faucet();
+        await sleep(2000);
+        console.log('Gen account: '+Account.Instance().get_address());
+        console.log('Test related functions, please make sure that the account contains SUI test coins');
+    }
+
     //await test_call()
     //await airdrop();
     await e_commerce();
